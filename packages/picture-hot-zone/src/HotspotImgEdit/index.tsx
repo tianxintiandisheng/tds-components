9b97263baafbd54b3215159b5e3db114e8f79b96
“田迪生” <disheng.tds@digitalexpo.com>
2022-08-02 13:04:09 +0800

    test

--- packages/picture-hot-zone/src/HotspotImgEdit/index.tsx
+++ packages/picture-hot-zone/src/HotspotImgEdit/index.tsx
@@ -59,7 +59,7 @@ function HotspotImgEdit(props: __Props) {
     typeComp,
   } = props;
   const showModal = () => {
-    console.log('测试0.4.9'); //todo delete
+    console.log('测试0.4.10'); //todo delete
     setCurSelectItemUuid(0); // 重置当前选中
     setIsModalVisible(true);
   };
